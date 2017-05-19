2aa008aa3c947fbafae41ddc52d771581f344669
Jon Willemain <jon.willemain@gmail.com>
2017-05-19 11:25:54 -0600

    working with heroku

--- knexfile.js
+++ knexfile.js
@@ -11,10 +11,20 @@ module.exports = {
       directory: './db/seeds/dev'
     }
   },
-
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true,
+    seeds: {
+      directory: './db/seeds/dev'
+    }
+  },
   test: {
     client: 'pg',
-    connection: 'postgres://localhost/byobtestdb',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/byobtestdb',
     migrations: {
       directory: './db/migrations'
     },
